dcd4d0e4b970440bbfe74d1d4fee5210f3cce4f1
alirezaAsadii <alirezaasadi7484@gmail.com>
2024-05-04 23:28:04 +0330

    create userRouter.js

---
+++ routes/userRouter.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const express = require("express");
+const router = express.Router();
+
+const userController = require("./../controllers/userControllers");
+router
+  .route("/")
+  .get(userController.getAllUser)
+  .post(userController.createUser);
+
+router
+  .route("/:id")
+  .get(userController.getUser)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);
+
+router.route("/:username/:password").post(userController.loginUser);
+
+module.exports = router;
