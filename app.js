e78ce9e2e6b2a0780c3009b4b1afbd8e144730c6
alirezaAsadii <alirezaasadi7484@gmail.com>
2024-05-04 23:29:15 +0330

    create app.js

--- app.js
+++ app.js
@@ -3,9 +3,22 @@
 const express = require("express");
 const morgan = require("morgan");
 
+const userRouter = require("./routes/userRouter");
+
+
 const app = express();
 
 //* middleware
+
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log("Hello from the middleware!");
+  next();
+});
+
+
+
+app.use("/digikala/users", userRouter);
 
 process.env.NODE_ENV === "development" && app.use(morgan("dev"));
 
