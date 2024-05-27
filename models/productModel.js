e10c93cc2fe4f730096a5cce985a2d7d1ba15735
alirezaAsadii <alirezaasadi7484@gmail.com>
2024-05-27 21:50:53 +0330

    create and developing productModel

---
+++ models/productModel.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const mongoose = require("mongoose");
+const { productSchema } = require("./../schemas/productSchema");
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
