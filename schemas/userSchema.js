58688a6dc329ac231c0bc0b0df3e110a8d5a1cf1
alirezaAsadii <alirezaasadi7484@gmail.com>
2024-05-30 23:45:42 +0330

    update userSchema

--- schemas/userSchema.js
+++ schemas/userSchema.js
@@ -6,18 +6,34 @@ const userSchema = new mongoose.Schema({
   firstName: {
     type: String,
     trim: true,
+    minlength: [3, "A username must have more or equal then 15 characters"],
+    maxlength: [
+      15,
+      "A user firstName must have less or equal then 15 characters",
+    ],
   },
   lastName: {
     type: String,
     trim: true,
+    minlength: [3, "A username must have more or equal then 15 characters"],
+    maxlength: [
+      15,
+      "A user lastName must have less or equal then 15 characters",
+    ],
   },
   username: {
     type: String,
     required: [true, "A user most have username"],
     unique: true,
+    minlength: [6, "A username must have more or equal then 6 characters"],
+    maxlength: [20, "A username must have less or equal then 20 characters"],
   },
   rule: {
     type: String,
+    enum: {
+      values: ["user", "admin"],
+      message: "You can only enter: user and admin",
+    },
   },
   email: {
     type: String,
@@ -29,6 +45,8 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     trim: true,
+    minlength: [8, "A password must have more or equal then 8 characters"],
+    maxlength: [20, "A password must have less or equal then 20 characters"],
   },
   phoneNumber: {
     type: Number,
