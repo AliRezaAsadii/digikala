953dd022bcd79eb12b51bbcc0d6fcad5a5fcfc97
alirezaAsadii <alirezaasadi7484@gmail.com>
2024-06-01 01:14:29 +0330

    create categoruSchema

---
+++ schemas/categorySchema.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const mongoose = require("mongoose");
+
+const categorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "a category must have name"],
+    trim: true,
+    unique: true,
+  },
+  cover: {
+    type: String,
+    required: [true, "a category must have cover"],
+    unique: true,
+    trim: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+exports.categorySchema = categorySchema
